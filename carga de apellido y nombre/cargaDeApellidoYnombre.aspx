717f7e248175ed1b58ce00b67f074a534d01613f
eliseooca <eliseo.oca29@gmail.com>
2023-09-05 16:23:46 -0300

    Agregar archivos de proyecto.

---
+++ carga de apellido y nombre/cargaDeApellidoYnombre.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="cargaDeApellidoYnombre.aspx.cs" Inherits="carga_de_apellido_y_nombre.cargaDeApellidoYnombre" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="txtNombre" runat="server" placeholder="Nombre" ></asp:TextBox>
+            <asp:TextBox ID="txtApellido" runat="server" placeholder="Apellido"></asp:TextBox>
+            <asp:Button ID="btnVerificar" runat="server" Text="Verificar" OnClick="btnVerificar_Click" />
+        </div>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Label ID="lblResultado" runat="server" ></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ carga de apellido y nombre/cargaDeApellidoYnombre.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace carga_de_apellido_y_nombre
+{
+    public partial class cargaDeApellidoYnombre : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnVerificar_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text.Trim();
+            string apellido = txtApellido.Text.Trim();
+            if (string.IsNullOrEmpty(nombre) || string.IsNullOrEmpty(apellido))
+            {
+                lblResultado.Text = "Por favor, ingrese tanto el Nombre como el apellido.";
+            }
+            else
+            {
+                lblResultado.Text = "Los datos se han ingresado correctamente.";
+            }
+        }
+    }
+}
